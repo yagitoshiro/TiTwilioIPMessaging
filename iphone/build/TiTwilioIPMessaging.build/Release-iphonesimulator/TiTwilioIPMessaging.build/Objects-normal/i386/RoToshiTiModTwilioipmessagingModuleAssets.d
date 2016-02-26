d89cb8b8850d107116185d166bfc96173218e220
Toshiro Yagi <yagitoshiro@gmail.com>
2016-02-26 17:16:29 +0900

    init

--- iphone/build/TiTwilioIPMessaging.build/Release-iphonesimulator/TiTwilioIPMessaging.build/Objects-normal/i386/RoToshiTiModTwilioipmessagingModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/toshiro/Documents/Appcelerator_Studio_Workspace/TiTwilioIPMessaging/iphone/Classes/RoToshiTiModTwilioipmessagingModuleAssets.m \
-  /Users/toshiro/Documents/Appcelerator_Studio_Workspace/TiTwilioIPMessaging/iphone/Classes/RoToshiTiModTwilioipmessagingModuleAssets.h
